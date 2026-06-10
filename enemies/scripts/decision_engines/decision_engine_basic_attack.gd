e1cff0fb6f8f527d0af671008333d8be5cf50aaf
ColeW96 <cole.whitaker18@gmail.com>
2026-06-10 16:56:22 -0400

    Add enemy - Living Armor

---
+++ enemies/scripts/decision_engines/decision_engine_basic_attack.gd
@@ -0,0 +1,31 @@
+class_name DecisionEngineBasicAttack
+extends DecisionEngine
+
+# Included in DecisionEngine:
+# var enemy : Enemy
+# var current_state : EnemyState
+# var blackboard : Blackboard
+
+
+func _ready() -> void:
+	await super()
+	pass
+
+
+# All the conditions for making decisions go in this function
+func decide() -> EnemyState:
+	# Example decisions
+	#if blackboard.damage_source:
+		#if blackboard.health <= 0:
+			#return es_death
+		#else:
+			#return es_stun
+	
+	if current_state is ESDeath or not blackboard.can_decide:
+		return null
+	
+	#if blackboard.target:
+		#if blackboard.distance_to_target < 40:
+			#return attack_state?
+		#return change_state?
+	return null # default state
